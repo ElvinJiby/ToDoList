f0194bcd99582549602a520ed1bd460ed51b1e83
Elvin Jiby <elvinjiby04@gmail.com>
2025-03-14 14:02:10 +0000

    program pause on error + fix on input

--- ToDoList.cs
+++ ToDoList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,6 +51,7 @@ namespace IntroToC_
             if (tasks.Contains(task))
             {
                 Console.WriteLine("Task already exists");
+                Thread.Sleep(3000);
                 return;
             }
             tasks.Add(task);
@@ -60,6 +62,7 @@ namespace IntroToC_
             if (id > size || id < 1)
             {
                 Console.WriteLine("Invalid task id.");
+                Thread.Sleep(3000);
                 return;
             }
             tasks.RemoveAt(id - 1);
@@ -70,6 +73,7 @@ namespace IntroToC_
             if (id > size || id < 1)
             {
                 Console.WriteLine("Invalid task id.");
+                Thread.Sleep(3000);
                 return;
             }
             tasks[id - 1].setName(name);
@@ -81,100 +85,19 @@ namespace IntroToC_
             if (id > size || id < 1)
             {
                 Console.WriteLine("Invalid task id.");
+                Thread.Sleep(3000);
                 return;
             }
             tasks[id - 1].markCompleted();
         }
-
-        public int getSize() { return size; }
-
         public void clearList()
         {
             tasks.Clear();
             size = 0;
         }
-
-        public void displayList()
-        {
-            if (size == 0)
-            {
-                Console.WriteLine("No tasks in the list. Try adding some!");
-                return;
-            }
-
-            Console.WriteLine("Tasks in the list:");
-            foreach (var item in tasks)
-            {
-                Console.WriteLine(item.getId() + ". " + item.getName());
-                Console.WriteLine("\t" + item.getDescription());
-                if (item.getIsCompleted()) Console.WriteLine("\tCompleted");
-                else Console.WriteLine("\tNot completed\n");
-            }
-        }
-
-        public bool printPrompt()
-        {
-            Console.WriteLine("1. Add new task");
-            Console.WriteLine("2. Remove a task");
-            Console.WriteLine("3. Update a task");
-            Console.WriteLine("4. Mark a task as completed");
-            Console.WriteLine("5. Clear list");
-            Console.WriteLine("6. Exit");
-
-            Console.WriteLine("Enter your choice:");
-            int choice = Convert.ToInt32(Console.ReadKey());
-
-            while (choice < 1 || choice > 6)
-            {
-                Console.WriteLine("Invalid choice. Please try again.");
-                choice = Convert.ToInt32(Console.ReadKey());
-            }
-
-            switch (choice)
-            {
-                case 1: addTaskPrompt(); break;
-                case 2: removeTaskPrompt(); break;
-                case 3: updateTaskPrompt(); break;
-                case 4: markTaskCompletedPrompt(); break;
-                case 5: clearList(); break;
-                case 6: return true;
-            }
-            return false;
-        }
-
-        public void addTaskPrompt()
-        {
-            Console.WriteLine("Enter task name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter task description:");
-            string description = Console.ReadLine();
-            addTask(name, description);
-        }
-
-        public void removeTaskPrompt()
-        {
-            Console.WriteLine("Enter task id:");
-            int id = Convert.ToInt32(Console.ReadKey());
-            removeTask(id);
-        }
-
-        public void updateTaskPrompt()
-        {
-            Console.WriteLine("Enter task id:");
-            int id = Convert.ToInt32(Console.ReadKey());
-            Console.WriteLine("Enter new task name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter new task description:");
-            string description = Console.ReadLine();
-            updateTask(id, name, description);
-        }
-
-        public void markTaskCompletedPrompt()
-        {
-            Console.WriteLine("Enter task id:");
-            int id = Convert.ToInt32(Console.ReadKey());
-            markTaskCompleted(id);
-        }
+        public bool isEmpty() { return size == 0; }
+        public List<Task> GetTasks() { return tasks; }
+        public int getSize() { return size; }
     }
 
     class Program
@@ -186,11 +109,92 @@ namespace IntroToC_
 
             while (!exit)
             {
-                list.displayList();
+                displayList(list);
                 Console.WriteLine("__________________________________________________\n");
-                exit = list.printPrompt();
+                exit = printPrompt(list);
                 Console.Clear();
             }
+        }
+        public static void displayList(ToDoList list)
+        {
+            if (list.isEmpty())
+            {
+                Console.WriteLine("No tasks in the list. Try adding some!");
+                return;
+            }
+
+            Console.WriteLine("Tasks in the list:");
+            foreach (var item in list.GetTasks())
+            {
+                Console.WriteLine(item.getId() + ". " + item.getName());
+                Console.WriteLine("\t" + item.getDescription());
+                if (item.getIsCompleted()) Console.WriteLine("\tCompleted");
+                else Console.WriteLine("\tNot completed\n");
+            }
+        }
+
+        public static bool printPrompt(ToDoList list)
+        {
+            Console.WriteLine("1. Add new task");
+            Console.WriteLine("2. Remove a task");
+            Console.WriteLine("3. Update a task");
+            Console.WriteLine("4. Mark a task as completed");
+            Console.WriteLine("5. Clear list");
+            Console.WriteLine("6. Exit");
+
+            Console.WriteLine("Enter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            while (choice < 1 || choice > 6)
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+                choice = Convert.ToInt32(Console.ReadLine());
+            }
+
+            switch (choice)
+            {
+                case 1: addTaskPrompt(list); break;
+                case 2: removeTaskPrompt(list); break;
+                case 3: updateTaskPrompt(list); break;
+                case 4: markTaskCompletedPrompt(list); break;
+                case 5: list.clearList(); break;
+                case 6: return true;
+            }
+            return false;
+        }
+
+        public static void addTaskPrompt(ToDoList list)
+        {
+            Console.WriteLine("Enter task name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter task description:");
+            string description = Console.ReadLine();
+            list.addTask(name, description);
+        }
+
+        public static void removeTaskPrompt(ToDoList list)
+        {
+            Console.WriteLine("Enter task id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            list.removeTask(id);
+        }
+
+        public static void updateTaskPrompt(ToDoList list)
+        {
+            Console.WriteLine("Enter task id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter new task name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter new task description:");
+            string description = Console.ReadLine();
+            list.updateTask(id, name, description);
+        }
+
+        public static void markTaskCompletedPrompt(ToDoList list)
+        {
+            Console.WriteLine("Enter task id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            list.markTaskCompleted(id);
         }
     }
 }
