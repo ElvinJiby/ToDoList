adeceb6508cb0c60af665dc2979acaf4c95ef3d6
Elvin Jiby <elvinjiby04@gmail.com>
2025-03-14 13:46:24 +0000

    to do list implemented

--- ToDoList.cs
+++ ToDoList.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace IntroToC_
 {
@@ -11,14 +12,13 @@ namespace IntroToC_
         private int id;
         private string name;
         private string description;
-        private bool isCompleted;
+        private bool isCompleted = false;
 
         public Task(int id, string name, string description)
         {
             this.id = id;
             this.name = name;
             this.description = description;
-            this.isCompleted = false;
         }
 
         public void markCompleted() {
@@ -28,15 +28,70 @@ namespace IntroToC_
         public int getId() { return id; }
         public string getName() { return name; }
         public string getDescription() { return description; }
+        public bool getIsCompleted() { return isCompleted; }
 
         public void setName(string name) { this.name = name; }
         public void setDescription(string description) { this.description = description; }
     }
     public class ToDoList
     {
-        public static void Main(String[] args)
+        private List<Task> tasks;
+        private int size;
+
+        public ToDoList()
         {
-            Console.WriteLine("test");
+            tasks = new List<Task>();
+            size = 0;
+        }
+
+        public void addTask(string name, string description)
+        {
+            Task task = new Task(size + 1, name, description);
+            if (tasks.Contains(task))
+            {
+                Console.WriteLine("Task already exists");
+                return;
+            }
+            tasks.Add(task);
+            size++;
+        }
+        public void removeTask(int id)
+        {
+            if (id > size || id < 1)
+            {
+                Console.WriteLine("Invalid task id.");
+                return;
+            }
+            tasks.RemoveAt(id - 1);
+            size--;
+        }
+        public void updateTask(int id, string name, string description)
+        {
+            if (id > size || id < 1)
+            {
+                Console.WriteLine("Invalid task id.");
+                return;
+            }
+            tasks[id - 1].setName(name);
+            tasks[id - 1].setDescription(description);
+        }
+
+        public void markTaskCompleted(int id)
+        {
+            if (id > size || id < 1)
+            {
+                Console.WriteLine("Invalid task id.");
+                return;
+            }
+            tasks[id - 1].markCompleted();
+        }
+
+        public int getSize() { return size; }
+
+        public void clearList()
+        {
+            tasks.Clear();
+            size = 0;
         }
     }
 }
